24418d77ccd65da98eb3a0179afd161a82074ad6
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-06-02 21:12:30 +0200

    Add example

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NLog;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private static Logger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <inheritdoc />
+        public Class1()
+        {
+            Logger.Info("Init class1");
+        }
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ClassLibrary1;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace Console_with_class_library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var class1 = new Class1();
+            Console.WriteLine("Press any key ...");
+            Console.ReadLine();
+        }
+    }
+}
