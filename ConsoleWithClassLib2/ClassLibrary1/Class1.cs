80afa1a32973cadad6e3dc93a8f8bc9bda1bfb65
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-06-10 22:32:37 +0200

    Added new demo

---
+++ ConsoleWithClassLib2/ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+
+        /// <inheritdoc />
+        public Class1()
+        {
+            // setup config
+            var configuration = new LoggingConfiguration();
+            configuration.AddRuleForAllLevels(new ConsoleTarget());
+            LogManager.Configuration = configuration;
+
+            // log
+            Logger logger = LogManager.GetCurrentClassLogger();
+            logger.Info("Init class1");
+        }
+    }
+}
