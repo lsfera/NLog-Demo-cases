45951f3c4f2c013e329429883009b8b2bf1d9b29
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-06-25 23:13:04 +0200

    Added aspnetcore2 example

---
+++ AspNetCore2WithConfigSetting/AspNetCore2WithConfigSetting/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCore2WithConfigSetting.Models;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCore2WithConfigSetting.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        /// <inheritdoc />
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Hi from home index");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
