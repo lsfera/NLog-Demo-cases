9ec8759fc90eb019523c93072fd9da96b63a7c3e
Julian Verdurmen <304NotModified@users.noreply.github.com>
2020-03-30 23:44:51 +0200

    Demo NLog.Mailkit

---
+++ NLogMailkit/NLogMailKitConsole/NLogMailKitConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NLog;
+
+namespace NLogMailKitConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetLogger("logger1");
+            logger.Error("Hi from main()");
+            LogManager.Shutdown();
+            Console.WriteLine("Log written");
+        }
+    }
+}
