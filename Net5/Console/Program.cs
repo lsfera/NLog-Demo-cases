7d16de784ff5325d2377610e3d33faa6e6957491
YOOX\giordanol <luca.giordano@ynap.com>
2021-03-21 09:34:08 +0100

    add example project

---
+++ Net5/Console/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using Host = Microsoft.Extensions.Hosting.Host;
+using ILogger = NLog.ILogger;
+
+namespace Console
+{
+    static class Program
+    {
+        private const String ServiceName = "Priamus";
+
+        static Program()
+        {
+            AppDomain.CurrentDomain.UnhandledException += HandleUnhandledException;
+            TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
+        }
+
+        private static (IConfiguration configuration, Logger logger) Configure()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+            NLog.Extensions.Logging.ConfigSettingLayoutRenderer.DefaultConfiguration = configuration;
+            var logger = LogManager.Setup()
+                .SetupExtensions(s => s.AutoLoadAssemblies(false))
+                .SetupExtensions(s => s.RegisterConfigSettings(configuration))
+                .LoadConfigurationFromSection(configuration)
+                .GetCurrentClassLogger();
+            return (configuration, logger);
+        }
+
+        private static void HandleUnhandledException(Object sender, UnhandledExceptionEventArgs e) =>
+            Configure().logger.Fatal((Exception) e.ExceptionObject, ServiceName);
+
+        private static void OnUnobservedTaskException(Object? sender, UnobservedTaskExceptionEventArgs e) =>
+            Configure().logger.Fatal(e.Exception.GetBaseException(), ServiceName);
+
+        private static async Task Main(String[] args)
+        {
+            var (configuration, logger) = Configure();
+            await CreateHostBuilder(args, configuration, logger)
+                .Build()
+                .RunAsync()
+                .ConfigureAwait(false);
+        }
+
+        private static IHostBuilder CreateHostBuilder(String[] args, IConfiguration config, ILogger logger) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((_, services)
+                    => services.AddHostedService(_ => BuildService(config, logger)))
+                .ConfigureLogging((context, builder) =>
+                {
+
+                    // configure Logging with NLog
+                    builder.ClearProviders();
+
+                    var section = context.Configuration.GetSection("NLog");
+                    var cfg = new NLogLoggingConfiguration(section);
+                    builder.AddNLog(cfg);
+                })
+                .UseWindowsService();
+        
+        private static Service BuildService(IConfiguration config, ILogger logger) => new(config, logger);
+    }
+}
---
+++ Net5/Console/Service.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using NLog;
+
+namespace Console
+{
+    public sealed class Service : IHostedService, IDisposable
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger _logger;
+        private readonly CancellationTokenSource _cancellationTokenSource;
+
+        public Service(IConfiguration configuration, ILogger logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+            _logger.Info("ServiceStarted");
+        }
+
+
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.Info("ServiceStopped");
+            _cancellationTokenSource.Cancel();
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _cancellationTokenSource.Dispose();
+
+            try
+            {
+
+                LogManager.Shutdown();
+            }
+            catch
+            {
+                //shallow
+            }
+        }
+
+    }
+}
